fa5dc99a8f5775323aa65baee829a82ab1f1205f
Kevin McGillivray <kmcgillivray@insightcreative.com>
2015-05-14 15:59:50 -0500

    Sync male and female firebase data.

--- fun.js
+++ fun.js
@@ -1,12 +1,16 @@
 var app = angular.module("occupied", ["firebase"]);
 
 app.controller('light', ['$scope', '$firebaseObject', function($scope, $firebaseObject) {
-  var ref = new Firebase("https://insight-occupied.firebaseio.com/occupied");
-  var syncObject = $firebaseObject(ref);
-  syncObject.$bindTo($scope, "occupied");
+  var womenRef = new Firebase("https://insight-occupied.firebaseio.com/women");
+  var womenSync = $firebaseObject(womenRef);
+  womenSync.$bindTo($scope, "women");
 
-  $scope.changeStatus = function() {
-    $scope.occupied.$value = $scope.occupied.$value === false ? true: false;
+  var menRef = new Firebase("https://insight-occupied.firebaseio.com/men");
+  var menSync = $firebaseObject(menRef);
+  menSync.$bindTo($scope, "men");
+
+  $scope.changeStatus = function(gender) {
+    gender.$value = gender.$value === false ? true: false;
   };
 
 }]);
